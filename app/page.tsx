53f188e0f498d2cc8af15bc556910b7311103c41
Dylan Tsao <92745437+dylangtsao@users.noreply.github.com>
2023-08-03 20:36:21 -0700

    responsive intro part complete.

--- app/page.tsx
+++ app/page.tsx
@@ -4,92 +4,42 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.backdrop}></div>
+      {/* nav-bar + mobile-nav */}
+      <section>
+        <div className={styles.nav}>
+          <h1 className={styles.title}>Apple Card</h1>
+          <div className={styles.nav_right}>
+            <div className={styles.dropdown}>
+              <div className={styles.dropdown_line}></div>
+              <div className={styles.dropdown_line}></div>
+              <div className={styles.dropdown_line}></div>
+            </div>
+            <div className={styles.nav_links}>
+              <a>Overview</a>
+              <a>Features</a>
+              <a>Family</a>
+              <a>Monthly Installments</a>
+              <a>How-To Videos</a>
+            </div>
+            <div className={styles.apply_backdrop}>
+              <h2 className={styles.apply}>Apply now</h2>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      {/* intro */}
+      <section>
+        <div className={styles.intro}>
+          <Image src="/intro_title.svg" width={320} height={95} alt="apple card" className={styles.intro_svg}></Image>
+          <div className={styles.intro_subtitle_backdrop}>
+            <a className={styles.intro_subtitle}>Apply now</a>
+          </div>
+        </div>
+        <div className={styles.subtitle_backdrop}>
+          <a>With every purchase, get up 3% Daily Cash back to spend or save. <a href="./page.tsx" className={styles.subtitle_link}>Apply now</a>{' >'}</a>
+        </div>
+      </section>
     </main>
   )
 }
