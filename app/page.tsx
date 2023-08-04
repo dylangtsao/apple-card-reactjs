fa5a85b539afe692454114ec1796b23a8561052d
Dylan Tsao <92745437+dylangtsao@users.noreply.github.com>
2023-08-04 14:23:57 -0700

    finished first section with responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,20 @@ export default function Home() {
           </div>
         </div>
         <div className={styles.subtitle_backdrop}>
-          <a>With every purchase, get up 3% Daily Cash back to spend or save. <a href="./page.tsx" className={styles.subtitle_link}>Apply now</a>{' >'}</a>
+          <a>With every purchase, get up 3% Daily Cash back to spend or save. <a className={styles.subtitle_link}>Apply now</a>{' >'}</a>
+        </div>
+      </section>
+      {/* First header */}
+      <section>
+        <div className={styles.first_header}>
+          <h1>The simplicity of Apple. <br></br>In a credit card.</h1>
+          <p>With Apple Card, we completely reinvented the credit card. 
+            Your information lives on your iPhone, beautifully laid out and easy to understand. 
+            We eliminated fees1 and built tools to help you pay less interest,
+             and you can apply in minutes to see if you are approved with no impact to your credit score.
+             Advanced technologies like Face ID, Touch ID, and Apple Pay give you an enhanced level 
+             of privacy and security. And with every purchase you get Daily Cash back that you can spend 
+             or save. Apple Card. It&apos;s everything a credit card should be.</p>
         </div>
       </section>
     </main>
